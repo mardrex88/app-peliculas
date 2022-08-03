fbd8aa1143ffdad71a5cd5359584e9cb22d9c09e
Marlon Vargas <marlon.vargas@sofka.com.co>
2022-08-02 20:20:24 -0500

    117. Peticiones HTTP - 118. Mapear una petición HTTP

--- lib/models/Movie.dart
+++ lib/models/Movie.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 
 class Movie {
   Movie({
-    required this.adult,
+    this.adult,
     this.backdropPath,
-    required this.genreIds,
-    required this.id,
+    this.genreIds,
+    this.id,
     required this.originalLanguage,
     required this.originalTitle,
-    required this.overview,
+    this.overview,
     required this.popularity,
     this.posterPath,
     this.releaseDate,
@@ -18,38 +18,24 @@ class Movie {
     required this.voteCount,
   });
 
-  bool adult;
+  bool? adult;
   String? backdropPath;
-  List<int> genreIds;
-  int id;
+  List<int>? genreIds;
+  int? id;
   String originalLanguage;
   String originalTitle;
-  String overview;
+  String? overview;
   double popularity;
   String? posterPath;
-  String? releaseDate;
+  DateTime? releaseDate;
   String title;
   bool video;
   double voteAverage;
   int voteCount;
 
-  String? heroId;
-
-  get fullPosterImg {
-    if (this.posterPath != null)
-      return 'https://image.tmdb.org/t/p/w500${this.posterPath}';
-
-    return 'https://i.stack.imgur.com/GNhxO.png';
-  }
-
-  get fullBackdropPath {
-    if (this.backdropPath != null)
-      return 'https://image.tmdb.org/t/p/w500${this.backdropPath}';
-
-    return 'https://i.stack.imgur.com/GNhxO.png';
-  }
-
   factory Movie.fromJson(String str) => Movie.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
 
   factory Movie.fromMap(Map<String, dynamic> json) => Movie(
         adult: json["adult"],
@@ -61,10 +47,28 @@ class Movie {
         overview: json["overview"],
         popularity: json["popularity"].toDouble(),
         posterPath: json["poster_path"],
-        releaseDate: json["release_date"],
+        releaseDate: DateTime.parse(json["release_date"]),
         title: json["title"],
         video: json["video"],
         voteAverage: json["vote_average"].toDouble(),
         voteCount: json["vote_count"],
       );
+
+  Map<String, dynamic> toMap() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "genre_ids": List<dynamic>.from(genreIds!.map((x) => x)),
+        "id": id,
+        "original_language": originalLanguage,
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date":
+            "${releaseDate!.year.toString().padLeft(4, '0')}-${releaseDate!.month.toString().padLeft(2, '0')}-${releaseDate?.day.toString().padLeft(2, '0')}",
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
 }
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,12 +1,29 @@
+import 'dart:convert';
+
+import 'package:app/models/now_playing_response.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '411c0a6d64b43f364012815b33f2e9a0';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('Movies Provider Constructor');
-    this.getOnDisplayMovies();
+    getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('Movies Provider getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(nowPlayingResponse.results[0].title);
   }
 }
