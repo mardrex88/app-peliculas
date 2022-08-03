5b8516ebc87f29284aa057b615052719a7c9259c
Marlon Vargas <marlon.vargas@sofka.com.co>
2022-08-03 14:06:59 -0500

    130. Mostrar las películas basado en el query

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,13 +1,17 @@
+import 'package:app/models/models.dart';
+import 'package:app/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
+  @override
   String get searchFieldLabel => 'Buscar Pelicula';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.clear),
+        icon: const Icon(Icons.clear),
         onPressed: () => query = '',
       )
     ];
@@ -15,31 +19,69 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
     return IconButton(
-      icon: Icon(Icons.arrow_back),
+      icon: const Icon(Icons.arrow_back),
       onPressed: () => close(context, null),
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
     return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     if (query.isEmpty) {
-      return Container(
-        child: Center(
-          child: Icon(
-            Icons.movie_creation_outlined,
-            size: 100,
-          ),
+      return const Center(
+        child: Icon(
+          Icons.movie_creation_outlined,
+          size: 100,
         ),
       );
     }
-    return Container();
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+        future: moviesProvider.searchMovies(query),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return _emptyContainer();
+
+          final movies = snapshot.data as List<Movie>;
+
+          return ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (context, index) => _MovieItem(movies[index]));
+        });
+  }
+
+  Widget _emptyContainer() {
+    return const Center(
+      child: Icon(
+        Icons.movie_creation_outlined,
+        size: 100,
+      ),
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: FadeInImage(
+          placeholder: const AssetImage('assets/img/no-image.jpg'),
+          image: NetworkImage(movie.fullPosterImg),
+        ),
+        title: Text(movie.title),
+        subtitle: Text(movie.overview!),
+        onTap: () {
+          Navigator.pushNamed(context, 'details', arguments: movie);
+        });
   }
 }
